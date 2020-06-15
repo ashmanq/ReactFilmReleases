a3bb6ae8ea4aad5adfce46c6f0fe1e1a656b66ac
Ashir <ashirq@outlook.com>
2020-06-15 16:05:34 +0100

    Created basic FilmBox component.

---
+++ src/containers/FilmBox.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FilmBox extends Component {
+  render() {
+    return (
+      <h2> I am the FilmBox component!</h2>
+    );
+  }
+};
+
+export default FilmBox;
